c3ebd1144a04d57788635e770dd5c734e87fbc78
utkarsh-vaishnav <utkarshsv93@gmail.com>
2024-08-10 23:08:22 +0530

    Added | reversal and palindrome functionality

--- reversalAndPalindrome/src/main.rs
+++ reversalAndPalindrome/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
+fn reverse_string(s: &str) -> String {
+    s.chars().rev().collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut data = String::new();
+    println!("Enter word or string you want: ");
+    io::stdin().read_line(&mut data).expect("Failed to read line");
+    let data = data.trim();
+    let reverse_data = reverse_string(data);
+    if data == reverse_data{
+        println!("Data is palindrome");
+    } else {
+        println!("Data is not palindrome");
+    }
+    println!("String: {data} & Reversed String: {reverse_data}");
 }
