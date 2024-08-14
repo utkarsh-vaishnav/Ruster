b5da72e24f9dd3817aac7b4da1d66899d14af986
utkarsh-vaishnav <utkarshsv93@gmail.com>
2024-08-14 22:20:09 +0530

    Added |Dynamic logic for fibonacci series

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -13,6 +13,27 @@ use std::io;
 //     }
 
 // Iterative function
+// fn fibonacci(n: u32) -> u32 {
+//     if n == 0 {
+//         return 0;
+//     } else if n == 1 {
+//         return 1;
+//     }
+
+//     let mut a = 0;
+//     let mut b = 1;
+//     let mut c = 0;
+
+//     for _ in 2..=n {
+//         c = a + b;
+//         a = b;
+//         b = c;
+//     }
+//     // println!("{} {} {}", a, b, c);
+//     c
+// }
+
+// Dynamic programming
 fn fibonacci(n: u32) -> u32 {
     if n == 0 {
         return 0;
@@ -20,17 +41,13 @@ fn fibonacci(n: u32) -> u32 {
         return 1;
     }
 
-    let mut a = 0;
-    let mut b = 1;
-    let mut c = 0;
+    let mut fib = vec![0; (n + 1) as usize];
+    fib[1] = 1;
 
-    for _ in 2..=n {
-        c = a + b;
-        a = b;
-        b = c;
+    for i in 2..=n as usize {
+        fib[i] = fib[i - 1] + fib[i - 2];
     }
-    // println!("{} {} {}", a, b, c);
-    c
+    fib[n as usize]
 }
 
 fn main() {
