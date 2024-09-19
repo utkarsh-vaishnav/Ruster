a1f74c2717b81297c5d2b1692259507daa4d494d
utkarsh-vaishnav <utkarshsv93@gmail.com>
2024-09-19 23:59:02 +0530

    Added | diamond_pattern

--- pattern_problems/diamond_pattern/src/main.rs
+++ pattern_problems/diamond_pattern/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the number of rows: ");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut rows).expect("Failed to read input");
+    let input = input.trim().parse::<i32>().unwrap();
+
+    
+
+
+
 }
