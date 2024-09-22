583cdd26def33726d046939cdf0068e68bc23163
utkarsh-vaishnav <utkarshsv93@gmail.com>
2024-09-23 00:50:35 +0530

    Added | logic of square_one_pattern

--- pattern_problems/square_one_pattern/src/main.rs
+++ pattern_problems/square_one_pattern/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the number of rows:");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let input = input.trim().parse::<i32>().unwrap();
+    println!("Your Pattern:");
+    for _each in 1..=input{
+        for _each in 1..=input{
+            print!("1 ");
+        }
+    println!();
+    }
 }
