f4c50c252479621fef967c6d30849de958aa55d1
utkarsh-vaishnav <utkarshsv93@gmail.com>
2024-09-23 00:46:26 +0530

    Added | square_star_pattern

---
+++ pattern_problems/square_star_pattern/src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+fn main() {
+    println!("Enter the number of rows:");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let input = input.trim().parse::<i32>().unwrap();
+    for _each in 1..=input{
+        for _each in 1..=input{
+            print!(" * ");
+        }
+    println!();
+    }
+}
