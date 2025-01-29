ee9e805988eb392da328ca6ec7771f28e90beb36
utkarsh-vaishnav <utkarshsv93@gmail.com>
2025-01-30 00:01:13 +0530

    Added | countdown timer

--- countdown_timer/src/main.rs
+++ countdown_timer/src/main.rs
@@ -1,3 +1,24 @@
+use std::{thread, time::Duration};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter countdown time (in seconds) :");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    
+    let seconds: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input! Please enter a number.");
+            return;
+        }
+    };
+
+    for i in (1..=seconds).rev() {
+        println!("{}", i);
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    println!("⏰ Time's up!");
 }
